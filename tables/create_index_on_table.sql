43c776b217df745f2b315884232ba92444a918d8
atmosmps <atmos.mps@gmail.com>
2020-02-07 11:17:26 -0300

    Adicionando novas informações sobre criaçãi de indices de campos que não são primary keys.

--- tables/create_index_on_table.sql
+++ tables/create_index_on_table.sql
@@ -1,12 +1,17 @@
 use db_name
 go
 
---//sintaxe para criacao de indice, fora do script de criacao de tabelas.
---//padrao de nomenclatura de indices segue a estrutura a seguir:
---//id
---//nome do esquema
---//nome da tabela
---//numeracao do indice(opcional)
+--// Sintaxe para criação de índices, fora do script de criacao de tabelas.
+--// padrao de nomenclatura de índices segue a estrutura a seguir:
 
-create index id_schema_name_tbl_name_000 on tbl_name(campo01, campo02)
+--// id
+--// nome do esquema
+--// nome da tabela
+--// numeracao do indice(opcional)
+
+create index id_schema_name_tbl_name_000 on tbl_name(campo_id)
+go
+
+--// Quando o índice for de um campo que não é um ID o prefixo deverá ser "ix".
+create index ix_schema_name_tbl_name_000 on tbl_name(campo01, campo02)
 go
